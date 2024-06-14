2c46375de5075135352a867f98b612018f410c48
mei <mei28aquarius@gmail.com>
2024-06-14 10:30:23 +0900

    feat: Extend luminate.nvim with undo/redo highlights
    This commit introduces a significant extension to the luminate.nvim
    plugin. Previously, the plugin only supported highlighting yanked and
    pasted text. Now, it also supports highlighting text that has been
    undone or redone.
    
    The README.md file has been updated to reflect these changes. The
    installation instructions now include the new 'VeryLazy' event. The
    configuration options have been expanded to include settings for undo
    and redo highlights, such as the highlight group, background and
    foreground colors, threshold for highlighting, keybindings, and
    enable/disable flags.
    
    These changes provide users with more visual feedback during editing,
    making it easier to track changes and navigate the text. The new
    features are optional and can be enabled or disabled individually,
    allowing users to customize the plugin to their liking.

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
 # luminate.nvim
 
-🌟 Highlight yanked and pasted text in Neovim with a splash of color.
+🌟 Highlight yanked, pasted, and undone/redone text in Neovim with a splash of color.
 
-<!-- ![demo image here](path_to_demo_image.gif)  <!-- If you have a demo GIF --> 
-
+<!-- ![demo image here](path_to_demo_image.gif)  <!-- If you have a demo GIF -->
 ## 📦 Installation
 
 Using [lazy.nvim](https://github.com/folke/lazy.nvim):
@@ -11,7 +10,7 @@ Using [lazy.nvim](https://github.com/folke/lazy.nvim):
 ```lua
 return {
     'mei28/luminate.nvim',
-    event = { 'TextYankPost' },
+    event = { 'VeryLazy' },
     config = function()
         require'luminate'.setup({
             -- if you want to customize, see Usage!
@@ -25,11 +24,43 @@ Simply call the setup function in your Neovim configuration:
 
 ```lua
 require'luminate'.setup({
-  timeout = 500,                     -- Duration of the highlight in milliseconds. Default is 500.
-  ctermbg = 8,                       -- Terminal background color. Default is 8 (gray).
-  guibg = "#ebcb8b",                 -- Background color for GUIs like Neovim-Qt or gVim. Default is "#ebcb8b".
-  highlight_on_yank = true,          -- Whether to highlight on yank. Default is true.
-  highlight_on_paste = true          -- Whether to highlight on paste. Default is true.
+  duration = 500,                     -- Duration of the highlight in milliseconds. Default is 500.
+  yank = {
+    hlgroup = "LuminateYank",         -- Highlight group for yanked text. Default is "LuminateYank".
+    guibg = "#2d4f67",                -- Background color for GUIs. Default is "#2d4f67".
+    fg = "#ebcb8b",                   -- Foreground color. Default is "#ebcb8b".
+    enabled = true,                   -- Enable highlight on yank. Default is true.
+  },
+  paste = {
+    hlgroup = "LuminatePaste",        -- Highlight group for pasted text. Default is "LuminatePaste".
+    guibg = "#2d4f67",                -- Background color for GUIs. Default is "#2d4f67".
+    fg = "#ebcb8b",                   -- Foreground color. Default is "#ebcb8b".
+    HIGHLIGHT_THRESHOLD = 0.9,        -- Threshold for highlighting pasted text. Default is 0.9.
+    enabled = true,                   -- Enable highlight on paste. Default is true.
+  },
+  undo = {
+    hlgroup = "HighlightUndo",        -- Highlight group for undo. Default is "HighlightUndo".
+    guibg = "#2d4f67",                -- Background color for GUIs. Default is "#2d4f67".
+    fg = "#ebcb8b",                   -- Foreground color. Default is "#ebcb8b".
+    HIGHLIGHT_THRESHOLD = 0.9,        -- Threshold for highlighting undone text. Default is 0.9.
+    mode = 'n',                       -- Mode in which to map undo. Default is 'n' (normal mode).
+    lhs = 'u',                        -- Keybinding for undo. Default is 'u'.
+    map = 'undo',                     -- Command to execute for undo. Default is 'undo'.
+    opts = {},                        -- Options for keymap. Default is {}.
+    enabled = true,                   -- Enable highlight on undo. Default is true.
+  },
+  redo = {
+    hlgroup = "HighlightRedo",        -- Highlight group for redo. Default is "HighlightRedo".
+    guibg = "#2d4f67",                -- Background color for GUIs. Default is "#2d4f67".
+    fg = "#ebcb8b",                   -- Foreground color. Default is "#ebcb8b".
+    HIGHLIGHT_THRESHOLD = 0.9,        -- Threshold for highlighting redone text. Default is 0.9.
+    mode = 'n',                       -- Mode in which to map redo. Default is 'n' (normal mode).
+    lhs = '<C-r>',                    -- Keybinding for redo. Default is '<C-r>'.
+    map = 'redo',                     -- Command to execute for redo. Default is 'redo'.
+    opts = {},                        -- Options for keymap. Default is {}.
+    enabled = true,                   -- Enable highlight on redo. Default is true.
+  },
+  highlight_for_count = true,         -- Highlight for count in undo/redo. Default is true.
 })
 ```
 
@@ -45,21 +76,68 @@ Then, specify the custom group in the setup function:
 
 ```lua
 require'luminate'.setup({
-  higroup = "MyYankGroup",
-  cterm = "red",
-  guibg = "#FF0000"
+  yank = {
+    hlgroup = "MyYankGroup",
+    guibg = "#FF0000",
+    fg = "red",
+  }
 })
-
 ```
 
 ### Enable/Disable Features:
 
 ```lua
 require'luminate'.setup({
-  highlight_on_yank = false,          -- Disable highlight on yank.
-  highlight_on_paste = true           -- Enable highlight on paste.
+  yank = {
+    enabled = false,          -- Disable highlight on yank.
+  },
+  paste = {
+    enabled = true,           -- Enable highlight on paste.
+  },
+  undo = {
+    enabled = true,           -- Enable highlight on undo.
+  },
+  redo = {
+    enabled = true,           -- Enable highlight on redo.
+  }
 })
 ```
+
+### ⚙️ Configuration Options
+
+* duration: Time in milliseconds for the highlight to last.
+* yank: Configuration for yank highlight.
+    * hlgroup: The highlight group to use for yank.
+    * guibg: Background color for GUIs.
+    * fg: Foreground color.
+    * enabled: Enable or disable yank highlight.
+* paste: Configuration for paste highlight.
+    * hlgroup: The highlight group to use for paste.
+    * guibg: Background color for GUIs.
+    * fg: Foreground color.
+    * HIGHLIGHT_THRESHOLD: Skip highlight if pasted text exceeds this fraction of total lines.
+    * enabled: Enable or disable paste highlight.
+* undo: Configuration for undo highlight.
+    * hlgroup: The highlight group to use for undo.
+    * guibg: Background color for GUIs.
+    * fg: Foreground color.
+    * HIGHLIGHT_THRESHOLD: Skip highlight if undone text exceeds this fraction of total lines.
+    * mode: Mode in which to map undo.
+    * lhs: Keybinding for undo.
+    * map: Command to execute for undo.
+    * opts: Options for keymap.
+    * enabled: Enable or disable undo highlight.
+* redo: Configuration for redo highlight.
+    * hlgroup: The highlight group to use for redo.
+    * guibg: Background color for GUIs.
+    * fg: Foreground color.
+    * HIGHLIGHT_THRESHOLD: Skip highlight if redone text exceeds this fraction of total lines.
+    * mode: Mode in which to map redo.
+    * lhs: Keybinding for redo.
+    * map: Command to execute for redo.
+    * opts: Options for keymap.
+    * enabled: Enable or disable redo highlight.
+* highlight_for_count: Highlight for count in undo/redo.
 
 ## 📜 License
 MIT
--- lua/luminate/init.lua
+++ lua/luminate/init.lua
@@ -1,55 +1,256 @@
-local M = {}
+local api = vim.api
 
-M.config = {
-  higroup = "LuminateYank",
-  timeout = 500,
-  ctermbg = 8,
-  guibg = "#ebcb8b",
-  highlight_on_yank = true,
-  highlight_on_paste = true,
-  HIGHLIGHT_THRESHOLD = 0.9
+local M = {
+  config = {
+    duration = 500,
+    timer = (vim.uv or vim.loop).new_timer(),
+    should_detach = true,
+    current_hlgroup = nil,
+    yank = {
+      hlgroup = "LuminateYank",
+      guibg = '#2d4f67',
+      fg = '#ebcb8b',
+      enabled = true,
+    },
+    paste = {
+      hlgroup = "LuminatePaste",
+      guibg = '#2d4f67',
+      fg = '#ebcb8b',
+      HIGHLIGHT_THRESHOLD = 0.9,
+      enabled = true,
+    },
+    undo = {
+      hlgroup = 'HighlightUndo',
+      guibg = '#2d4f67',
+      fg = '#ebcb8b',
+      HIGHLIGHT_THRESHOLD = 0.9,
+      mode = 'n',
+      lhs = 'u',
+      map = 'undo',
+      opts = {},
+    },
+    redo = {
+      hlgroup = 'HighlightRedo',
+      guibg = '#2d4f67',
+      fg = '#ebcb8b',
+      HIGHLIGHT_THRESHOLD = 0.9,
+      mode = 'n',
+      lhs = '<C-r>',
+      map = 'redo',
+      opts = {},
+    },
+    highlight_for_count = true,
+  },
+  namespaces = {
+    yank = api.nvim_create_namespace('LuminateYankHighlight'),
+    paste = api.nvim_create_namespace('LuminatePasteHighlight'),
+    undo = api.nvim_create_namespace('HighlightUndoNamespace'),
+    redo = api.nvim_create_namespace('HighlightRedoNamespace'),
+  },
 }
 
-
-M.setup = function(opts)
-  M.config = vim.tbl_deep_extend('force', M.config, opts or {})
-  vim.cmd(string.format('highlight %s ctermbg=%s guibg=%s', M.config.higroup, M.config.ctermbg, M.config.guibg))
-
-  vim.cmd('augroup LuminateHighlight')
-  vim.cmd('autocmd!')
-  if M.config.highlight_on_yank then
-    vim.cmd("autocmd TextYankPost * lua require'luminate'.on_yank()")
-  end
-  if M.config.highlight_on_paste then
-    vim.cmd("autocmd TextChanged * lua require'luminate'.on_paste()")
-  end
-  vim.cmd('augroup END')
+-- 汎用的なハイライト設定
+local function set_highlight(name, params)
+  api.nvim_set_hl(0, name, params)
 end
 
-M.on_yank = function()
-  vim.highlight.on_yank({ higroup = M.config.higroup, timeout = M.config.timeout })
+-- オートコマンドの設定
+local function set_autocmds()
+  api.nvim_create_augroup('LuminateHighlight', { clear = true })
+
+  if M.config.yank.enabled then
+    api.nvim_create_autocmd('TextYankPost', {
+      group = 'LuminateHighlight',
+      callback = function() M.on_yank() end
+    })
+  end
+
+  if M.config.paste.enabled then
+    api.nvim_create_autocmd('TextChanged', {
+      group = 'LuminateHighlight',
+      callback = function() M.attach_bytes_highlight('paste') end
+    })
+  end
+
+  if M.config.undo.enabled or M.config.redo.enabled then
+    api.nvim_create_autocmd('BufEnter', {
+      group = 'LuminateHighlight',
+      callback = function() M.attach_bytes_highlight('undo_redo') end
+    })
+  end
 end
 
-M.on_paste = function()
-  local start_line = vim.api.nvim_buf_get_mark(0, "[")[1]
-  local end_line = vim.api.nvim_buf_get_mark(0, "]")[1]
-  local start_col = vim.api.nvim_buf_get_mark(0, "[")[2]
-  local end_col = vim.api.nvim_buf_get_mark(0, "]")[2]
-  local total_lines = vim.api.nvim_buf_line_count(0)
-
-  -- 変更された行数が全体の5%を超えた場合にハイライトをスキップ
-  if (end_line - start_line) / total_lines > M.config.HIGHLIGHT_THRESHOLD then
-    return
+-- バイト変更の処理
+function M.on_bytes(event_type, bufnr, changedtick,
+                    start_row, start_column,
+                    byte_offset, old_end_row,
+                    old_end_col, old_end_byte,
+                    new_end_row, new_end_col, new_end_byte)
+  if M.should_detach then
+    return true
   end
 
-  local ns_id = vim.api.nvim_create_namespace('LuminatePasteHighlight')
-  vim.highlight.range(0, ns_id, M.config.higroup, { start_line - 1, start_col }, { end_line - 1, end_col })
+  local config = M.config[event_type]
+  local num_lines = api.nvim_buf_line_count(bufnr)
+  local end_row = start_row + new_end_row
+  local end_col = start_column + new_end_col
 
-  if M.config.timeout > 0 then
-    vim.defer_fn(function()
-      vim.api.nvim_buf_clear_namespace(0, ns_id, 0, -1)
-    end, M.config.timeout)
+  if end_row >= num_lines then
+    end_col = #api.nvim_buf_get_lines(bufnr, -2, -1, false)[1]
   end
+
+  -- ハイライト適用
+  vim.schedule(function()
+    if (end_row - start_row) / num_lines <= config.HIGHLIGHT_THRESHOLD then
+      vim.highlight.range(
+        bufnr,
+        M.namespaces[event_type],
+        config.hlgroup,
+        { start_row, start_column },
+        { end_row, end_col }
+      )
+      -- ハイライトのクリア
+      M.defer_clear_highlights(bufnr, M.namespaces[event_type])
+    end
+  end)
+end
+
+-- バッファアタッチ
+function M.attach_bytes_highlight(event_type)
+  M.should_detach = false
+  api.nvim_buf_attach(0, false, {
+    on_bytes = function(ignored, bufnr, changedtick,
+                        start_row, start_column,
+                        byte_offset, old_end_row,
+                        old_end_col, old_end_byte,
+                        new_end_row, new_end_col, new_end_byte)
+      M.on_bytes(event_type, bufnr, changedtick,
+        start_row, start_column,
+        byte_offset, old_end_row,
+        old_end_col, old_end_byte,
+        new_end_row, new_end_col, new_end_byte)
+    end,
+  })
+end
+
+-- ヤンク後のハイライト
+function M.on_yank()
+  vim.highlight.on_yank({
+    higroup = M.config.yank.hlgroup,
+    timeout = M.config.duration,
+    namespace = M.namespaces.yank,
+  })
+end
+
+-- ハイライトのクリア（一定時間後）
+function M.defer_clear_highlights(bufnr, namespace)
+  vim.defer_fn(function()
+    api.nvim_buf_clear_namespace(bufnr, namespace, 0, -1)
+  end, M.config.duration)
+end
+
+-- 元のキーマップを実行
+function M.call_original_kemap(map)
+  if type(map) == 'string' then
+    vim.cmd(map)
+  elseif type(map) == 'function' then
+    map()
+  end
+end
+
+-- undo 操作時に fold を開く
+local function open_folds_on_undo()
+  if vim.tbl_contains(vim.opt.foldopen:get(), "undo") then
+    vim.cmd.normal({ "zv", bang = true })
+  end
+end
+
+-- 初期化関数
+function M.setup(config)
+  M.config = vim.tbl_deep_extend('keep', M.config, config or {})
+
+  -- yank/paste/undo/redo のハイライトの設定
+  set_highlight(M.config.yank.hlgroup, {
+    ctermbg = M.config.yank.ctermbg,
+    bg = M.config.yank.guibg,
+    fg = M.config.yank.fg,
+  })
+
+  set_highlight(M.config.paste.hlgroup, {
+    ctermbg = M.config.paste.ctermbg,
+    bg = M.config.paste.guibg,
+    fg = M.config.paste.fg,
+  })
+
+  set_highlight(M.config.undo.hlgroup, {
+    ctermbg = M.config.undo.ctermbg,
+    bg = M.config.undo.guibg,
+    fg = M.config.undo.fg,
+  })
+
+  set_highlight(M.config.redo.hlgroup, {
+    ctermbg = M.config.redo.ctermbg,
+    bg = M.config.redo.guibg,
+    fg = M.config.redo.fg,
+  })
+
+  set_autocmds()
+
+  -- undo/redo のキーマップ設定
+  local undo = M.config.undo
+  vim.keymap.set(undo.mode, undo.lhs, function()
+    if M.config.highlight_for_count or vim.v.count == 0 then
+      M.highlight_undo_redo('undo', function()
+        M.call_original_kemap(undo.map)
+      end)
+    else
+      local keys = api.nvim_replace_termcodes(vim.v.count .. 'u', true, false, true)
+      api.nvim_feedkeys(keys, 'n', false)
+    end
+    open_folds_on_undo()
+  end, undo.opts)
+
+  local redo = M.config.redo
+  vim.keymap.set(redo.mode, redo.lhs, function()
+    if M.config.highlight_for_count or vim.v.count == 0 then
+      M.highlight_undo_redo('redo', function()
+        M.call_original_kemap(redo.map)
+      end)
+    else
+      local keys = api.nvim_replace_termcodes(vim.v.count .. '<C-r>', true, false, true)
+      api.nvim_feedkeys(keys, 'n', false)
+    end
+    open_folds_on_undo()
+  end, redo.opts)
+end
+
+-- undo/redo のハイライト実行
+function M.highlight_undo_redo(event_type, command)
+  M.current_hlgroup = M.config[event_type].hlgroup
+  api.nvim_buf_attach(0, false, {
+    on_bytes = function(ignored, bufnr, changedtick,
+                        start_row, start_column,
+                        byte_offset, old_end_row,
+                        old_end_col, old_end_byte,
+                        new_end_row, new_end_col, new_end_byte)
+      M.on_bytes(event_type, bufnr, changedtick,
+        start_row, start_column,
+        byte_offset, old_end_row,
+        old_end_col, old_end_byte,
+        new_end_row, new_end_col, new_end_byte)
+    end,
+  })
+  M.should_detach = false
+
+  if M.config.highlight_for_count then
+    for _ = 1, vim.v.count1 do
+      command()
+    end
+  else
+    command()
+  end
+
+  M.should_detach = true
 end
 
 return M
