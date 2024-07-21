e37a02462cd445896df647b156598099db4c4a88
mei <mei28aquarius@gmail.com>
2024-07-21 10:17:07 +0900

    add

--- lua/luminate/highlight.lua
+++ lua/luminate/highlight.lua
@@ -1,6 +1,6 @@
 local api = vim.api
-local config = require('luminate.config').config
-local namespaces = require('luminate.config').namespaces
+local config_module = require('luminate.config')
+local namespaces = config_module.namespaces
 local M = {}
 
 function M.set_highlight(name, params)
@@ -9,11 +9,11 @@ end
 
 function M.on_bytes(event_type, bufnr, changedtick, start_row, start_column, byte_offset, old_end_row, old_end_col,
                     old_end_byte, new_end_row, new_end_col, new_end_byte)
-  if config.should_detach or vim.fn.mode() == 'i' then
+  if config_module.config.should_detach or vim.fn.mode() == 'i' then
     return true
   end
 
-  local event_config = config[event_type]
+  local event_config = config_module.config[event_type]
   local num_lines = api.nvim_buf_line_count(bufnr)
   local end_row = start_row + new_end_row
   local end_col = start_column + new_end_col
@@ -39,8 +39,7 @@ end
 function M.defer_clear_highlights(bufnr, namespace)
   vim.defer_fn(function()
     api.nvim_buf_clear_namespace(bufnr, namespace, 0, -1)
-  end, config.duration)
+  end, config_module.config.duration)
 end
 
 return M
-
