27a7d4f465abcdb7605e75a5245d18546e0693c1
mei <mei28aquarius@gmail.com>
2024-07-31 22:58:52 +0900

    fix(highlight): add buffer validity check before clearing
    Added a check to ensure the buffer is valid before attempting to clear
    highlights in the `defer_clear_highlights` function. This prevents
    potential errors when the buffer is no longer valid, improving the
    stability and reliability of the highlight clearing process.
    
    Co-authored-by: JayLCypher

--- lua/luminate/highlight.lua
+++ lua/luminate/highlight.lua
@@ -37,6 +37,7 @@ function M.on_bytes(event_type, bufnr, changedtick, start_row, start_column, byt
 end
 
 function M.defer_clear_highlights(bufnr, namespace)
+  if not api.nvim_buf_is_valid(bufnr) then return end
   vim.defer_fn(function()
     api.nvim_buf_clear_namespace(bufnr, namespace, 0, -1)
   end, config_module.config.duration)
